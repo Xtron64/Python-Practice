5b1a4b016d66dcfd9cf0e8c3830b4a85faa6e57e
Xtron64 <snekkieboi@pm.me>
2023-05-28 18:45:51 +0100

    Update hello_world/src/main.py

--- hello_world/src/main.py
+++ hello_world/src/main.py
@@ -1,8 +1,23 @@
-'''Obviosly don't need imports'''
-def main():
-    '''Doc-string to get pylint to shut up'''
+'''
+Obviosly don't need imports
+'''
+def main() -> int:
+    '''
+    Doc-string to get pylint to shut up
+    '''
     print("Hello, world!")
+    return 0
 
 
-if __name__ == '__main__':
-    main()
+while __name__ == '__main__':
+    '''
+    This executes the main() function and evaluates what to do with the exit code
+    '''
+    code: int = main()
+    if code == 0:
+        break
+    elif code == 1:
+        raise Exception("Exit code 1: Generic error.")
+    else:
+        raise Exception("No exit code.")
+        
